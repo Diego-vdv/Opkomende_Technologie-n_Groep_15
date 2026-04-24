d7767c0b6a0323593bb476f1c2d669eb887e3def
Ajuinnn <167209375+Ajuinnn@users.noreply.github.com>
2026-04-24 15:26:54 +0200

    Create Test Temperatuur + Luchtvochtigheid.ino

---
+++ Code/Test Temperatuur + Luchtvochtigheid.ino
@@ -0,0 +1,37 @@
+#include <DHT.h>
+
+const int DHT_PIN = A4;
+const int DHT_TYPE = DHT11;
+
+DHT dht(DHT_PIN, DHT_TYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  Serial.println("DHT test gestart");
+  Serial.println("Temperatuur en luchtvochtigheid:");
+  Serial.println();
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperatureC = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperatureC)) {
+    Serial.println("Fout: geen geldige DHT meting");
+    delay(2000);
+    return;
+  }
+
+  Serial.print("Temperatuur: ");
+  Serial.print(temperatureC, 1);
+  Serial.println(" C");
+
+  Serial.print("Luchtvochtigheid: ");
+  Serial.print(humidity, 1);
+  Serial.println(" %");
+
+  Serial.println();
+  delay(2000);
+}
