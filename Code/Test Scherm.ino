2c96c2d4bfc1599ff2794f1df34302d4047676d0
Diego-vdv <diego.vandevyvere@ugent.be>
2026-05-01 14:45:56 +0200

    Update Test Scherm.ino
    Code leesbaarder gemaakt

--- Code/Test Scherm.ino
+++ Code/Test Scherm.ino
@@ -1,25 +1,29 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_GC9A01A.h>
+
 /*
-  Simpelste test voor jouw ronde GC9A01 scherm.
-  Aangesloten volgens je foto:
-  - CS  -> D12
-  - DC  -> D11
-  - SDA -> D10
-  - SCL -> D9
-  - GND -> GND
-  - VCC -> 3.3V
-  - RST -> niet aangesloten
-  Omdat SDA/SCL in je foto niet op de standaard SPI-pinnen lijken te zitten,
-  gebruiken we hier software SPI.
+  Simpele test voor GC9A01 rond display (software SPI)
+  Aansluitingen:
+  CS   -> D12
+  DC   -> D11
+  MOSI -> D10
+  SCLK -> D9
+  VCC  -> 3.3V
+  GND  -> GND
 */
+
+//Pin Configuratie
 constexpr int TFT_CS = 12;
 constexpr int TFT_DC = 11;
 constexpr int TFT_MOSI = 10;
 constexpr int TFT_SCLK = 9;
-constexpr int TFT_RST = -1;
+constexpr int TFT_RST = -1; // Niet aangesloten
+
+// TFT object
 Adafruit_GC9A01A tft(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+// Functie: Smiley tekenen
 void drawHappySmiley() {
   tft.fillCircle(120, 120, 120, GC9A01A_YELLOW);
   tft.drawCircle(120, 120, 90, GC9A01A_WHITE);
@@ -31,11 +35,15 @@ void drawHappySmiley() {
     tft.drawLine(138, 156 + i, 158, 138 + i, GC9A01A_BLACK);
   }
 }
+
+// Setup
 void setup() {
   tft.begin();
   tft.setRotation(0);
   tft.fillScreen(GC9A01A_BLACK);
   drawHappySmiley();
 }
+
+// Loop (niet gebruikt)
 void loop() {
 }
